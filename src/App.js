0d3ec0d65e1b88d4f7529801ec422cff51f19854
Nixxo <92588172+Nekuso@users.noreply.github.com>
2022-06-22 02:46:55 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, {useState} from 'react';
 
+
 const App = () => {
-
-    const [currentCount, setCurrentCount] = useState(0);
-
-    function increment() {
-        setCurrentCount(currentCount + 1 )
-    }
-
-    function decrement() {
-        setCurrentCount(currentCount - 1 )
-    }
-
     return (
         <div>
-            <button onClick={increment}>+1</button>
-            <h1>{currentCount}</h1>
-            <button onClick={decrement}>-1</button>
         </div>
     );
 }
